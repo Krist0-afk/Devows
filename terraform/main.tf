5f386887a03ae6874e6cf127c2e4a7bf32366f3c
Tavotsu <123342265+Tavotsu@users.noreply.github.com>
2026-06-21 20:59:29 -0400

    cd: actualizado el main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -245,6 +245,13 @@ resource "aws_security_group" "db_sg" {
     security_groups = [aws_security_group.ecs_sg.id]
   }
 
+  ingress {
+    from_port       = 6379
+    to_port         = 6379
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ecs_sg.id]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
